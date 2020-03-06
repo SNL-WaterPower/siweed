70162cf0d096bce8bb3e1df47f1d29a742bf44d6
Ross <ncross@sandia.gov>
2020-03-06 16:57:59 -0700

    fixed some comments and tuned stepInterval and maxRate

--- Arduino/Stepper_with_encoder_correction/Stepper_with_encoder_correction.ino
+++ Arduino/Stepper_with_encoder_correction/Stepper_with_encoder_correction.ino
@@ -3,14 +3,14 @@
 #include<math.h>
 
 ////////////////////////////////////////////////
-//Derived funciton here:
+//Position funciton here:
 const float pi = 3.14159265358979;
 const float leadPitch = 10.0;     //mm/turn
 const float gearRatio = 40.0 / 12.0; //motor turns per lead screw turns
 const float motorStepsPerTurn = 400.0;   //steps per motor revolution
 const float encStepsPerTurn = 3200.0;   //steps per encoder revolution
-float amp = 10.0; //in mm
-float hz = 2.0;
+float amp = 20.0; //in mm
+float hz = 1.5;
 
 float inputFnc(float tm)   //inputs time in seconds //outputs velocity in mm/second
 {
@@ -20,11 +20,11 @@ float inputFnc(float tm)   //inputs time in seconds //outputs velocity in mm/sec
 
 Encoder waveEnc(2, 3);   //pins 2 and 3(interupts)//for 800 ppr/3200 counts per revolution set dip switches(0100) //2048ppr/8192 counts per revolution max(0000)
 const int stepPin = 4, dirPin = 5;
-float encPos = 0;   //how many steps have been taken acording to the encoder(in steps)
+float encPos = 0;   //distace travelled in mm
 unsigned long previousStepMillis;
 double t;    //time in seconds
-float stepInterval = .01;   //in seconds
-const float maxRate = 500.0;   //max mm/seconds
+float stepInterval = .05;   //in seconds  //min: .02
+const float maxRate = 400.0;   //max mm/seconds   //may need to be adjusted
 
 void setup()
 {
@@ -42,6 +42,7 @@ void loop()
 
   encPos = waveEnc.read() * (1 / encStepsPerTurn) * leadPitch; //steps*(turns/step)*(mm/turn)
   t = millis() / (float)1000;
+  Serial.println(encPos);
 
   unsigned long currentMillis = millis();
   if (currentMillis - previousStepMillis >= stepInterval)
@@ -52,14 +53,11 @@ void loop()
   }
 
 }
-float futurePos;
 void moveMotor()
 {
   float pos = encPos;
-  //Serial.println(pos);  
-  futurePos = inputFnc(t + stepInterval);  //time plus delta time
+  float futurePos = inputFnc(t + stepInterval);  //time plus delta time
   float vel = (futurePos - pos) / stepInterval; //desired velocity in mm/second
-  //Serial.println(encPos);
   if (vel > 0)
   {
     digitalWrite(dirPin, HIGH);
@@ -72,14 +70,14 @@ void moveMotor()
   if (sp < 2.6)    //31hz is the lowest frequency of tone(), in mm/s this is 2.6
   {
     sp = 2.6;
-    //Serial.println("min");
+    Serial.println("min");
   }
   else if(sp > maxRate)    //max speed
   {
     sp = maxRate;
     Serial.println("max");
   }
-  tone(stepPin, mmToSteps(sp));     //steps per second
+  tone(stepPin, mmToSteps(sp));
 }
 
 
