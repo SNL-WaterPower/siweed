22be9bef186d54b0fa8ce1c61b6693265e412678
Ross <ncross@sandia.gov>
2020-04-05 10:15:25 -0600

    added all modes to WEC_Controller

--- Arduino/WEC_Controller/WEC_Controller.ino
+++ Arduino/WEC_Controller/WEC_Controller.ino
@@ -3,17 +3,20 @@
 int mode = -1;    //-1 stop, 0 torque control, 1 feedback control, 2 function mode
 double t = 0;    //time in seconds
 float tau = 0, kp = 0, kd = 0, power = 0, vel = 0;
-const int tauPin = DAC0, l1Pin = 10, l2Pin = 11, l3Pin = 12, l4Pin = 13;
+float tauCommand = 0;   //tau after any modifications
+const int tauPin = DAC0, enablePin = 6, l1Pin = 10, l2Pin = 11, l3Pin = 12, l4Pin = 13;
 Encoder wecEnc(2, 3); //pins 2 and 3(interupts)//for 800 ppr/3200 counts per revolution set dip switches(0100) //2048ppr/8192 counts per revolution max(0000)
 float encPos;
+const float pi = 3.14159265358979;
 const float encStepsPerTurn = 3200.0;
 const float teethPerTurn = 5;   //EDIT
 const float mmPerTooth = 10;    //EDIT
+const float minTau = -5, maxTau = 5;    //EDIT
 
 const float interval = .001;    //interval of updateTau interupt in seconds
 const float serialInterval = .02; //interval of serial interupt
 
-int n = 1;            //numbe of components
+int n = 1;            //number of components
 const int maxComponents = 60;   //max needed number of frequency components
 float amps[maxComponents];
 float phases[maxComponents];
@@ -23,6 +26,8 @@ void setup()
 {
   Serial.begin(19200);
   analogWriteResolution(12);    //analog write now runs from 0 to 4095
+  pinMode(enablePin, OUTPUT);
+  digitalWrite(enablePin, LOW);
   wecEnc.write(0);     //zero encoder
 }
 
@@ -30,7 +35,7 @@ void loop()
 {
   encPos = wecEnc.read() * (1 / encStepsPerTurn) * teethPerTurn * mmPerTooth; //steps*(turns/step)*(mm/turn)
   t = micros() / 1000000.0;
-  power = -1 * tau * vel;
+  power = -1 * tauCommand * vel;
   readSerial();
 }
 float pos;
@@ -39,19 +44,42 @@ void updateTau()    //will need to be called by an interupt
   float prevPos = pos;
   pos = encPos;
   vel = (pos - prevPos) / interval;
-  analogWrite(tauPin, tau);   //writes the torque value to the dac
+  switch (mode)
+  {
+    case -1:
+      digitalWrite(enablePin, LOW);   //stop
+      break;
+    case 0:
+      tauCommand = tau;       //direct control
+      break;
+    case 1:
+      tauCommand = kp * pos + kd * vel;      //PD feedback control
+      break;
+    case 2:
+      tauCommand = 0;
+      for (int i = 0; i < n; i++)           //function mode
+      {
+        tauCommand += amps[i] * sin(2 * pi * t * freqs[i] + phases[i]);
+      }
+      break;
+  }
+  if (mode != -1)
+  {
+    analogWrite(tauPin, mapFloat(tauCommand, minTau, maxTau, 0, 4095));    //!!!!!!!NEEDS TO ACCOUNT FOR EDGE ZONES//sends to the motor controller after mapping from newtom/meters to analog
+    digitalWrite(enablePin, HIGH);
+  }
 }
 void sendSerial()   //will need to be called by an interupt
 {
   /*
     e: encoder position
-    t: tau
+    t: tau commanded to motor
     p: power
   */
   Serial.write('e');
   sendFloat(encPos);
   Serial.write('t');
-  sendFloat(tau);
+  sendFloat(tauCommand);
   Serial.write('p');
   sendFloat(power);
 
@@ -132,4 +160,8 @@ void sendFloat(float f)
   dataStr += String(f);
   dataStr += ">";    //end of string
   Serial.print(dataStr);
+}
+float mapFloat(long x, long in_min, long in_max, long out_min, long out_max)
+{
+  return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
 }
